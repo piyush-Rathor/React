eba83e277f748118924f5f4174c82b888f91338a
piyush-Rathor <psrathor16072000@gmail.com>
2020-09-01 13:24:25 -0700

    New Js Syntax For Fragment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component, Fragment } from 'react';
 class App extends Component{
     render(){
-        return(<Fragment>
+        return(<>
             <h1>Hey guys</h1>
             <h2>Hello dude</h2>
-        </Fragment>)
+        </>)
     }
 }
 export default App;
