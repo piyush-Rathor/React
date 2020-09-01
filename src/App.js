428beeadc63a95a1892c349a21eba9bf4137737c
piyush-Rathor <psrathor16072000@gmail.com>
2020-09-01 13:58:20 -0700

    adding Class using camel cse notation and add styl

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import "./App.css"
 import Student from './Student';
 const name=" Hey i am Variable"
 class App extends Component{
@@ -7,7 +8,7 @@ class App extends Component{
             return "hello I m from fx"
         }
         return(<>
-            <h1>Hey guys{10+20+30*3},{a()}</h1>
+            <h1 className="bg">Hey guys{10+20+30*3},{a()}</h1>
         <h2>Hello dude{name}</h2>
             <Student />
             <Student />
