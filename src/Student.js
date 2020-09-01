62b85ca86786daa0dc66ac326b11192846c95561
piyush-Rathor <psrathor16072000@gmail.com>
2020-09-01 14:20:44 -0700

    Adding typechecking

--- src/Student.js
+++ src/Student.js
@@ -1,5 +1,14 @@
 import React from 'react'
+import pt from "prop-types";
 const  Student=(props)=>{
-    return (<h1>I Am Student</h1>);
+    Student.prototype={
+        name:pt.string.isRequired,
+        roll:pt.number
+    };
+    Student.defaultProps={
+        name:"Geekyshows"
+    };
+return (<h1>I Am Student{pt.name}</h1>);
+    
 }
 export default Student;
