61fd80eb2c5ee8e58c932841d81ae08222e6fd90
piyush-Rathor <psrathor16072000@gmail.com>
2020-09-01 13:23:48 -0700

    redring multiple Eliment using Frsgment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 class App extends Component{
     render(){
-        return(<div>
+        return(<Fragment>
             <h1>Hey guys</h1>
-            <h2>Hello due</h2>
-        </div>)
+            <h2>Hello dude</h2>
+        </Fragment>)
     }
 }
 export default App;
