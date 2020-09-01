768d415427d23bbb1c7cc7432d115e1af5f86d93
piyush-Rathor <psrathor16072000@gmail.com>
2020-09-01 13:18:00 -0700

    rendering Multipul eliment using div

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 class App extends Component{
     render(){
-        return(<h1>Hey guys</h1>)
+        return(<div>
+            <h1>Hey guys</h1>
+            <h2>Hello due</h2>
+        </div>)
     }
 }
 export default App;
